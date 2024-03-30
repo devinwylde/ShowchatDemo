f4ca422efaf04eae3656a6845d3d7e52d812bad6
Devin Wylde <devinwylde@ufl.edu>
2024-03-29 22:12:18 -0400

    Initial Commit

---
+++ ShowchatDemo/ShowchatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowchatDemoApp.swift
+//  ShowchatDemo
+//
+//  Created by Devin Wylde on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ShowchatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
